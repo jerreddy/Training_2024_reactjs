8e7a97b7eec06d06890c5d3162f6917cf390bb9b
unknown <j.ramamohanreddy@gmail.com>
2024-07-08 21:50:25 +0530

    react and springboot

---
+++ SpringBoot_Jpa_MySql/src/main/java/com/example/demo/repository/TutorialRepository.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.example.demo.repository;
+
+/**
+ * @author USER
+ *
+ */
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Tutorial;
+
+
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
+}
